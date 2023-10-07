43979a00681d1c4138590e722fd9212468c99fd1
Mariya B <supermariyabros@gmail.com>
2023-10-07 22:19:12 +0300

    🚀 Add Service Section! 🔥

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,6 +3,7 @@ import About from './About/About';
 import Skills from './Skills/Skills';
 import FeaturedPosts from './Blog/FeaturedPosts';
 import Projects from './Projects/Projects';
+import Service from './Services/Service';
 
 const MainPage = () => {
   return (
@@ -10,6 +11,7 @@ const MainPage = () => {
         <HomePage />
         <About />
         <Skills />
+        <Service />
         <FeaturedPosts />
         <Projects />
     </div>
