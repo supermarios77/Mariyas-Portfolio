34a5bd552c64097b9f046fafcba7640ba23afe6f
Mariya B <supermariyabros@gmail.com>
2023-10-28 15:26:52 +0300

    update

--- src/components/Blog/BlogPost.jsx
+++ src/components/Blog/BlogPost.jsx
@@ -18,31 +18,103 @@ const BlogPost = () => {
   return (
     <section className="blog__post container section">
 
-        <h3 className="post__title">{post.title}</h3>
+      <h3 className="post__title">{post.title}</h3>
 
-        <p className="post__details">{post.author} / {post.category}</p>
+      <p className="post__details">{post.author} / {post.category}</p>
 
-        <p className="post__summary">{post.summary}</p>
+      <p className="post__summary">{post.summary}</p>
 
-        <img src={post.image} alt={post.title} className="post__img" />
+      <img src={post.image} alt={post.title} className="post__img" />
 
-        <h2 className="Section">{post.section}</h2>
+      <h2 className="Section">{post.section}</h2>
 
-        <p className="paragraph">
-          {post.paragraph}
-        </p>
+      <p className="paragraph">
+        {post.paragraph}
+      </p>
 
-        <h2 className="Section">{post.section2}</h2>
+      <h2 className="Section">{post.section2}</h2>
 
-        <p className="paragraph">
-          {post.paragraph2}
-        </p>
+      <p className="paragraph">
+        {post.paragraph2}
+      </p>
 
-        <h2 className="Section">{post.section3}</h2>
+      <h2 className="Section">{post.section3}</h2>
 
-        <p className="paragraph">
-          {post.paragraph3}
-        </p>
+      <p className="paragraph">
+        {post.paragraph3}
+      </p>
+
+      <h2 className="Section">{post.section4}</h2>
+
+      <p className="paragraph">
+        {post.paragraph4}
+      </p>
+
+      <h2 className="Section">{post.section5}</h2>
+
+      <p className="paragraph">
+        {post.paragraph5}
+      </p>
+
+      <h2 className="Section">{post.section6}</h2>
+
+      <p className="paragraph">
+        {post.paragraph6}
+      </p>
+
+      <h2 className="Section">{post.section7}</h2>
+
+      <p className="paragraph">
+        {post.paragraph7}
+      </p>
+
+      <h2 className="Section">{post.section8}</h2>
+
+      <p className="paragraph">
+        {post.paragraph8}
+      </p>
+
+      <h2 className="Section">{post.section9}</h2>
+
+      <p className="paragraph">
+        {post.paragraph9}
+      </p>
+
+      <h2 className="Section">{post.section10}</h2>
+
+      <p className="paragraph">
+        {post.paragraph10}
+      </p>
+
+      <h2 className="Section">{post.section11}</h2>
+
+      <p className="paragraph">
+        {post.paragraph11}
+      </p>
+
+      <h2 className="Section">{post.section12}</h2>
+
+      <p className="paragraph">
+        {post.paragraph12}
+      </p>
+
+      <h2 className="Section">{post.section13}</h2>
+
+      <p className="paragraph">
+        {post.paragraph13}
+      </p>
+
+      <h2 className="Section">{post.section14}</h2>
+
+      <p className="paragraph">
+        {post.paragraph14}
+      </p>
+
+      <h2 className="Section">{post.section15}</h2>
+
+      <p className="paragraph">
+        {post.paragraph15}
+      </p>
 
     </section>
   )
--- src/components/Sidebar/Bar.jsx
+++ src/components/Sidebar/Bar.jsx
@@ -7,69 +7,78 @@ const SideBar = () => {
   const copyright_end = copyright + 3;
 
   return (
-    <aside className="aside">
+    <>
+      <aside className="aside">
 
-      <a href="#home" className="nav__logo">
-        <img src={Logo} alt="logo" />
-      </a>
+        <a href="#home" className="nav__logo">
+          <img src={Logo} alt="logo" />
+        </a>
 
-      <nav className="nav">
-        <div className="nav__menu">
-          <ul className="nav__list">
+        <nav className="nav">
+          <div className="nav__menu">
+            <ul className="nav__list">
 
-            <li className="nav__item">
-              <a href="/" className="nav__link">
-                <i className="icon-home"></i>
-              </a>
-            </li>
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  <i className="icon-home"></i>
+                </a>
+              </li>
 
-            <li className="nav__item">
-              <a href="/#about" className="nav__link">
-                <i className="icon-user-following"></i>
-              </a>
-            </li>
+              <li className="nav__item">
+                <a href="/#about" className="nav__link">
+                  <i className="icon-user-following"></i>
+                </a>
+              </li>
 
-            <li className="nav__item">
-              <a href="/#services" className="nav__link">
-                <i className="icon-briefcase"></i>
-              </a>
-            </li>
+              <li className="nav__item">
+                <a href="/#services" className="nav__link">
+                  <i className="icon-briefcase"></i>
+                </a>
+              </li>
 
-            <li className="nav__item">
-              <a href="/#resume" className="nav__link">
-                <i className="icon-graduation"></i>
-              </a>
-            </li>
+              <li className="nav__item">
+                <a href="/#resume" className="nav__link">
+                  <i className="icon-graduation"></i>
+                </a>
+              </li>
 
-            <li className="nav__item">
-              <a href="/#projects" className="nav__link">
-                <i className="icon-layers"></i>
-              </a>
-            </li>
+              <li className="nav__item">
+                <a href="/#projects" className="nav__link">
+                  <i className="icon-layers"></i>
+                </a>
+              </li>
 
-            <li className="nav__item">
-              <a href="/blog" className="nav__link">
-                <i className="icon-note"></i>
-              </a>
-            </li>
+              <li className="nav__item">
+                <a href="/blog" className="nav__link">
+                  <i className="icon-note"></i>
+                </a>
+              </li>
 
-            <li className="nav__item">
-              <a href="/#contact" className="nav__link">
-                <i className="icon-bubble"></i>
-              </a>
-            </li>
+              <li className="nav__item">
+                <a href="/#contact" className="nav__link">
+                  <i className="icon-bubble"></i>
+                </a>
+              </li>
 
-          </ul>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="nav__footer">
+          <span className="copyright">
+            &copy; {copyright} - {copyright_end}
+          </span>
         </div>
-      </nav>
 
-      <div className="nav__footer">
-        <span className="copyright">
-         &copy; {copyright} - {copyright_end}
-        </span>
+      </aside>
+
+      <div className="nav__toggle">
+
+        <i className="icon-menu"></i>
+
+        
       </div>
-
-    </aside>
+    </>
   )
 }
 
